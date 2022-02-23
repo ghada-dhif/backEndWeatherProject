9e1e190f96ccfcf7e20208b9bd5d4ac9e6737188
Ghada Dhif <ghada1edhif@gmail.com>
2022-02-23 10:27:05 +0100

    display cities names

--- backEnd-weather-master/server.js
+++ backEnd-weather-master/server.js
@@ -1,36 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 const axios = require("axios").default;
-const cors = require('cors');
+const cors = require("cors");
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "http://localhost:8080",
-        methods: ["GET", "POST"]
-    }
-})
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static('public'))
-app.get('/', function (req, res) {
-    res.status(200).json("Backend server working properly! 🙌 ");
-})
-app.get('/json', function (req, res) {
-   res.status(200).json({"message":"ok"})
-})
-
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.status(200).json("Backend server working properly! 🙌 ");
+});
+app.get("/json", function (req, res) {
+  res.status(200).json({ message: "ok" });
+});
+let cities = [];
+getAllCities().then((res) => {
+  cities = res.data
+    .map((city) => {
+      return city.name.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+    })
+    .filter((item) => !item.includes(" "));
+});
 function getWeather(city) {
-    return axios({url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7416d45e0fc4254f5c8e7bbb442f0b47`})
+  return axios({
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7416d45e0fc4254f5c8e7bbb442f0b47`,
+  });
 }
 
-async function getAllCities(){
-    return  axios({url: `https://restcountries.com/v2/all`})
- }
+async function getAllCities() {
+  return axios({ url: `https://restcountries.com/v2/all` });
+}
 // Server start
 server.listen(3001, function () {
- console.log('Votre app est disponible sur localhost:3001 !')
-})
+  console.log("Votre app est disponible sur localhost:3001 !");
+});
